b3bab7960c94c8cb2e1a25c4b4fb1a698df25ebb
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-11-15 20:21:23 +0200

    set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Login from './pages/Login.jsx'
+import Contact from './pages/Contact.jsx'
+import PlaceOrder from './pages/PlaceOrder.jsx'
+import Product from './pages/Product.jsx'
+import Cart from './pages/cart.jsx'
+import Order from './pages/Order.jsx'
+import Collection from './pages/Collection.jsx'
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/orders' element={<Order/>} />
+        <Route path='/collection' element={<Collection/>} />
+      </Routes>
     </div>
   )
 }
