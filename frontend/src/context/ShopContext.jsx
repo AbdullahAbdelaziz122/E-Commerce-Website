be4073c7ca0a080d1eadb6f50332d010228bff08
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-11-16 12:07:26 +0200

    Created Shop Context

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { createContext, useState, useEffect } from "react";
 
-const ShopContext = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+    
+    const currency = '$';
+    const deliveryFee = 10;
+    
+
+    const [products, setProducts] = useState([]); 
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                
+                const response = await fetch('https://fakestoreapi.com/products');
+                const data = await response.json();
+                
+                
+                setProducts(data);
+                console.log(() => {data === null? console.log("Products fetched successfully") : console.log("No products found") }); 
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+
+        fetchProducts();
+    }, []); 
+
+    
+    const value = {
+        products,
+        currency,
+        deliveryFee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
 
-export default ShopContext
+export default ShopContextProvider;
