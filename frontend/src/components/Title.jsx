61b27d8e31b5a76c1c62b12e35f278a0c2225c2a
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-11-16 14:11:38 +0200

    created title component

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({ title }) => {
+  return (
+    <div className='inline-flex gap-2 items-center mt-20 mb-20 justify-center'>
+      <p className="font-bold text-4xl  md:text-5xl text-black font-['Alfa_Slab_One']">{title}</p>
+    </div>
+  )
+}
+
+export default Title
